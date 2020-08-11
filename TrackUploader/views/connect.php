2975a11b1a48ba5f64fcbb763decb62cbda4257f
svenbol <sven.bollaerts@hotmail.com>
2020-08-11 14:12:40 +0200

    connect to heroku connect
    Make it HEROKU readable

--- TrackUploader/views/connect.php
+++ TrackUploader/views/connect.php
@@ -1,23 +1,38 @@
 <?php
 
 /*
-$servername = "localhost";
-$username = "id7180973_admintracks";
-$password = "12345678";
-$database = "id7180973_id7180973_tracks";
+// 000webhostapp
+	$servername = "localhost";
+	$username = "id7180973_admintracks";
+	$password = "12345678";
+	$database = "id7180973_id7180973_tracks";
 */
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "tbl_tracks";
+/*
+// Local Computer
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$database = "tbl_tracks";
+	
+	// Create connection
+	$con = mysqli_connect($servername, $username, $password, $database);
+	
+	// Check connection
+	if ($con->connect_error) {
+	    die("Connection failed: " . $conn->connect_error);
+	}
+*/
 
+//Heroku 
 
-// Create connection
-$con = mysqli_connect($servername, $username, $password, $database);
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-// Check connection
-if ($con->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"]);
+
+$conn = new mysqli($server,$username,$password,$db);
+
 ?>
