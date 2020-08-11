56988d0b755c9f2c9bd8dc6b3319955d320a8a36
svenbol <sven.bollaerts@hotmail.com>
2020-08-11 17:53:42 +0200

    Update connect.php

--- TrackUploader/views/connect.php
+++ TrackUploader/views/connect.php
@@ -28,10 +28,10 @@
 
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"]);
+$server = "ec2-54-247-94-127.eu-west-1.compute.amazonaws.com"; //$url["host"];
+$username = "urpcpbtnnpqcuv"; // $url["user"];
+$password = "17caee8c9c7b4077b417cc5a405f8cbcdc4510a3b396bd651629aa74d4f0298b"; //$url["pass"];
+$db = "d9hlug852adfv9"; //substr($url["path"]);
 
 $conn = new mysqli($server,$username,$password,$db);
 
