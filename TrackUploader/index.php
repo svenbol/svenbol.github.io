39200bf4dd59608840813cf539e39ad16abf6d7b
svenbol <sven.bollaerts@hotmail.com>
2020-12-31 12:56:55 +0100

    KAPOT must commit

--- TrackUploader/index.php
+++ TrackUploader/index.php
@@ -4,19 +4,28 @@ ini_set("display_errors", 1);
 include_once "./classes/Pagina_data.class.php";
 $paginaData = new Pagina_Data();
 $paginaData->titel = "Track Uploader";
+<<<<<<< HEAD
 //$paginaData->favicon = include_once "./app/favicon.php";
 $paginaData->content = include_once "./views/navigatie.php";
+=======
+$paginaData->content = include "./views/navigatie.php";
+>>>>>>> parent of a546d79... rollback (?)
 
-$userClicked = isset($_GET["pagina"]);
-if($userClicked) $fileToLoad = $_GET["pagina"];
-else $fileToLoad ="gallerij";
+$userClicked = isset($_GET['pagina']);
+if($userClicked) $fileToLoad = $_GET['pagina'];
+else $fileToLoad ='gallerij';
 
 $paginaData->content .= include_once "./views/$fileToLoad.php";
+$paginaData->addCss('./css/styling.css');
 
+<<<<<<< HEAD
 //$paginaData->bootstrap = include_once "./app/bootstrapCSS.php";
 $paginaData->addCss("./css/styling.css");
 //$paginaData->bottomScripts = include_once "./app/bootstrapPlugin.php";
 
 $pagina = include './templates/pagina.php';
+=======
+$pagina = include_once "./templates/pagina.php";
+>>>>>>> parent of a546d79... rollback (?)
 echo $pagina;
 ?>
--- TrackUploader/templates/pagina.php
+++ TrackUploader/templates/pagina.php
@@ -1,19 +1,16 @@
 <?php
-return "
-<!DOCTYPE html>
+return "<!DOCTYPE html>
 <html>
 <head>
-	<title>$paginaData->titel</title>
-	<meta http-equiv='Content-Type' content='text/html;charset=utf-8' />
-	$paginaData->favicon
-	$paginaData->bootstrapCss
-	$paginaData->css
+<title>$paginaData->titel</title>
+<meta http-equiv='Content-Type' content='text/html;charset=utf-8' />
+$paginaData->css
+<link rel='icon' type='image/png' href='../img/favicon.png'>
 </head>
 <body>
-	<div class='bgcolor'>
-		$paginaData->content
-	</div>
-	$paginaData->bottomScripts
+<div class='bgcolor'>
+$paginaData->content
+</div>
 </body>
 </html>";
 ?> 
