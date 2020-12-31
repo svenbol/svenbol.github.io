dbb6315391a27927e6d3b057f9cc3a3361897e52
svenbol <sven.bollaerts@hotmail.com>
2020-12-31 13:03:30 +0100

    KANKERZOOI

--- TrackUploader/index.php
+++ TrackUploader/index.php
@@ -6,7 +6,7 @@ $paginaData = new Pagina_Data();
 $paginaData->titel = "Track Uploader";
 //$paginaData->favicon = include_once "./app/favicon.php";
 $paginaData->content = include_once "./views/navigatie.php";
-
+// toevoeging
 $userClicked = isset($_GET['pagina']);
 if($userClicked) $fileToLoad = $_GET['pagina'];
 else $fileToLoad ='gallerij';
@@ -17,10 +17,6 @@ $paginaData->addCss('./css/styling.css');
 $paginaData->addCss("./css/styling.css");
 //$paginaData->bottomScripts = include_once "./app/bootstrapPlugin.php";
 $pagina = include './templates/pagina.php';
-
-
-
-
 
 echo $pagina;
 ?>
