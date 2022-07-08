a4c501ccdaa86cb04aadb5729c8e7a26760a0f60
svenbol <sven.bollaerts@hotmail.com>
2022-07-08 09:15:49 +0200

    Update connect.php

--- TrackUploader/views/connect.php
+++ TrackUploader/views/connect.php
@@ -1,30 +1,5 @@
 <?php
-
-/*
-// 000webhostapp
-	$servername = "localhost";
-	$username = "id7180973_admintracks";
-	$password = "12345678";
-	$database = "id7180973_id7180973_tracks";
-*/
-
-/*
-// Local Computer
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$database = "tbl_tracks";
-	
-	// Create connection
-	$con = mysqli_connect($servername, $username, $password, $database);
-	
-	// Check connection
-	if ($con->connect_error) {
-	    die("Connection failed: " . $conn->connect_error);
-	}
-*/
-
-//Heroku ZZZ
+//Heroku
 
 $user  = "skJJZ961vs";
 $dbname = "skJJl961vs";
@@ -33,23 +8,4 @@ $server = "remotemysql.com";
 $port =	"3306";
 
 $con = new mysqli($server,$user,$pass,$db);
-
-//$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-/*
-$server = "postgres://inxmshuupeoyxo:f11bd96c6de474b35577cd6b470552c5d567efdd3e7c9599211b3d2d6c24528e@ec2-54-86-57-171.compute-1.amazonaws.com:5432/d2etsfdv58166o"; //$url["host"];
-$username = "inxmshuupeoyxo"; // $url["user"];
-$password = "f11bd96c6de474b35577cd6b470552c5d567efdd3e7c9599211b3d2d6c24528e"; //$url["pass"];
-$db = "d2etsfdv58166o"; //substr($url["path"]);
-*/
-
-
-// Heroku trackuploader
-/*
-$server = "remotemysql.com";
-$username = "GsN8DgJAZN";
-$password = "Wm5glX6EBD";
-$db = "GsN8DgJAZN";
-
-$conn = new mysqli($server,$username,$password,$db);
-*/
 ?>
